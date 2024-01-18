76dbbf7150dddec078c1f1e4fca9cd924cbe0754
Agustin Villar <jaguvillar@gmail.com>
2024-01-18 13:41:02 -0300

    feat: data ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
 // import { fetchCards } from "@lib/data";
 
-const cardIds = ["si1-1", "pop4-4"];
+const cardIds = [];
 
 interface Card {
   id: string;
@@ -19,10 +19,15 @@ interface Card {
 // ];
 
 async function fetchCards() {
-  const cards = await fetch("https://api.pokemontcg.io/v2/cards");
+  const cards = await fetch("https://api.pokemontcg.io/v2/cards?q=name:mew&pageSize=10");
   const cardsJson = await cards.json();
   console.log(cardsJson);
-  
+
+  const ids = cardsJson.data.map(obj => {
+    console.log(obj);
+
+  })
+
   return cardsJson.data;
 }
 
