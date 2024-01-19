0b4dced256977069dbe3a0d0bb7902fe63a5ce83
JAguVillar <jaguvillar@gmail.com>
2024-01-19 00:24:43 -0300

    feat: view transitions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,34 +1,34 @@
-// import { fetchCards } from "@lib/data";
-
 const cardIds = [];
-
 interface Card {
   id: string;
   name: string;
 }
 
-// export const cards: Card[] = [
-//   {
-//     id: "si1-1",
-//     name: "Sith Infiltrator",
-//   },
-//   {
-//     id: "pop4-4",
-//     name: "Naboo Royal Starship",
-//   },
-// ];
-
 async function fetchCards() {
-  const cards = await fetch("https://api.pokemontcg.io/v2/cards?q=name:mew&pageSize=10");
-  const cardsJson = await cards.json();
-  console.log(cardsJson);
+  const response = await fetch("https://api.pokemontcg.io/v2/cards?q=name:mew&pageSize=10");
+  const cardsJson = await response.json();
 
-  const ids = cardsJson.data.map(obj => {
-    console.log(obj);
-
-  })
-
-  return cardsJson.data;
+  const cards: Card[] = cardsJson.data.map(obj => ({ id: obj.id, name: obj.name }));
+  cards.forEach(element => {
+    cardIds.push({params: {id: element.id}});
+  });
+  console.log(cardIds);
+  
+  return cards; 
 }
 
-export { cardIds, fetchCards };
+async function iniciar() {
+  try {
+    const fetchedCards = await fetchCards();
+  } catch (error) {
+    console.error("Error fetcheando:", error);
+  }
+}
+
+iniciar();
+
+export const cards: Card[] = cardIds
+
+// export function example(){
+//     return [{ params: { id: "si1-1" } }, { params: { id: "pop4-4" } }]
+// }
